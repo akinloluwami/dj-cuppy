e93b9e46bd8507bcb481931c19a1a07bfa1f10d2
Akinkunmi <akinkunmiisrael41@gmail.com>
2021-01-23 15:44:27 +0000

    updated!

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
-const btnFollow = document.querySelector(".btn-follow");
+/*const btnFollow = document.querySelector(".btn-follow");
 const btnPfl = document.querySelector(".btn-pfl");
-const btnIcon = document.querySelector(".btn-icon");
+const btnIcon = document.querySelector(".btn-icon");*/
 const suggestion = document.querySelector(".suggestion");
 const bio = document.querySelector(".bio");
-const seeAll = document.querySelector(".all");
 const popUp = document.querySelector(".popup");
-const unFollow = document.querySelector(".unfollow-container");
-const unFollowBtn = document.querySelector(".unfollow-btn");
 const overLay = document.querySelector(".overlay");
-const cancel = document.querySelector(".cancel");
 const loader = document.querySelector(".loader");
 
-btnFollow.addEventListener("click", function () {
+/*btnFollow.addEventListener("click", function () {
   btnPfl.classList.add("active");
   btnFollow.innerHTML = "Message";
   btnFollow.classList.add("active");
@@ -19,30 +15,16 @@ btnFollow.addEventListener("click", function () {
 btnIcon.addEventListener("click", function () {
   suggestion.classList.toggle("active");
   btnIcon.classList.toggle("active");
-});
-seeAll.addEventListener("click", function () {
-  popUp.classList.add("active");
-  overLay.classList.add("active");
-});
+});*/
+
 overLay.addEventListener("click", function () {
   overLay.classList.remove("active");
   popUp.classList.remove("active");
 });
-btnPfl.addEventListener("click", function () {
+/*btnPfl.addEventListener("click", function () {
   overLay.classList.add("active");
   unFollow.classList.add("active");
-});
-cancel.addEventListener("click", function () {
-  overLay.classList.remove("active");
-  unFollow.classList.remove("active");
-});
-unFollowBtn.addEventListener("click", function () {
-  btnPfl.classList.remove("active");
-  btnFollow.innerHTML = "Follow";
-  unFollow.classList.remove("active");
-  overLay.classList.remove("active");
-  btnFollow.classList.remove("active");
-});
+});*/
 
 window.addEventListener("load", () => {
   setTimeout(() => {
